c2ac2fb66390be26e149eaf7c21699f0b7093051
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2016-12-05 16:45:56 +0000

    Criacao do projeto

---
+++ CastleWar/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Dmytro Yaremyshyn
+ *
+ * Created on 5 de Dezembro de 2016, 16:44
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
